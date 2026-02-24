8611cea1ac6a1e0d6648ff87ca6ef326c9a18bfc
Sara-Fioretti <fiorettisara22@gmail.com>
2026-02-24 17:07:17 +0100

    Creazione Card

--- assets/script.js
+++ assets/script.js
@@ -38,22 +38,60 @@ const teamMembers = [
 ];
 
 /*PROCEDIMENTO:
-1)  Aggancio la card container ad una variabile
+1)  Aggancio la team container ad una variabile
 2) Creo una funzione createCard
-  2.1)Faccio un ciclo for che va da i=0 a i<teamMembers.length
-  2.2) Destruttuto l'array e creo una variabile{name, role, email, img}
-  2.3) Creo un div cardContainer con il comando createElement
-  2.4) Creo un div imageContainer con il comando createElement
-  2.5) Creo una variabile memberImage, gli metto il source e l'appendo all'imageContainer
-  2.6) Creo un div textContainer con il comando createElement
-  2.7) Creo una variabile memberName e metto come innerText name
-  2.8) Creo una variabile memberRole e metto come innerText role
-  2.9) Creo una variabile memberEmail e metto come innerText email
-  2.10) Con il comando appendChild appendo memberName, memberRole e memberEmail al textContainer
-  2.11) Con il comando appendChild appendo il textContainer al cardContainer
-  2.12) Con il comando appendChild appendo il textContainer al teamContainer
+  2.1) Creo un div cardContainer con il comando createElement
+  2.2) Creo un div imageContainer con il comando createElement
+  2.3) Creo una variabile memberImage, gli metto il source e l'appendo all'imageContainer
+  2.4) Creo un div textContainer con il comando createElement
+  2.5) Creo una variabile memberName e metto come innerText name
+  2.6) Creo una variabile memberRole e metto come innerText role
+  2.7) Creo una variabile memberEmail e metto come innerText email
+  2.8) Con il comando appendChild appendo memberName, memberRole e memberEmail al textContainer
+  2.9) Con il comando appendChild appendo il textContainer al cardContainer
+  2.10) Con il comando appendChild appendo il textContainer al teamContainer
+3) Faccio un ciclo for che va da i=0 a i<teamMembers.length
+  3.1) Destruttuto l'array e creo una variabile{name, role, email, img}
+  3.2) Richiamo la funzione createCard
+  4) BONUS: Aggiungere un form di aggiunta membri che permetta di visualizzare il nuovo membro sulla pagina
+*/
 
-  3) BONUS: Aggiungere un form di aggiunta membri che permetta di visualizzare il nuovo membro sulla pagina
+teamContainer = document.querySelector(".team-container")
 
- 
- 
+function createCard(name, role, email, img) {
+  let cardContainer = document.createElement("div")
+  cardContainer.classList.add("team-card")
+
+  let imageContainer = document.createElement("div")
+  cardContainer.classList.add("card-image")
+
+  let memberImage = document.createElement("img")
+  memberImage.src = img
+  imageContainer.appendChild (memberImage)
+  cardContainer.appendChild (imageContainer)
+
+  let textContainer = document.createElement("div")
+  textContainer.classList.add("card-text")
+
+  let memberName = document.createElement ("h3")
+  memberName.innerText = name
+
+  let memberRole = document.createElement ("h4")
+  memberRole.innerText = role
+
+  let memberEmail = document.createElement ("p")
+  memberEmail.innerText = email
+
+  textContainer.appendChild(memberName)
+  textContainer.appendChild (memberRole)
+  textContainer.appendChild (memberEmail)
+  cardContainer.appendChild (textContainer)
+
+  teamContainer.appendChild (cardContainer)
+}
+
+for (let i = 0; i < teamMembers.length; i++) {
+  const { name, role, email, img } = teamMembers[i]
+  createCard (name, role, email, img)
+}
+
